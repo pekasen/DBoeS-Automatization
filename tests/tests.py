9a513b80e2036cba170fd58db046e39a7f9facb8
Mani Levian Asli <55234866+manilevian@users.noreply.github.com>
2020-08-22 22:37:48 +0200

    add comments
    added some comments for the first test

--- tests/tests.py
+++ tests/tests.py
@@ -11,11 +11,14 @@ class testing_wikifetcher_sachsen(unittest.TestCase):
 
 	def setUp(self):
 		options = Options()
+		# Change to 'true' to prevent a firefox window to be opened
 		options.headless = False
 		self.driver = webdriver.Firefox(options=options)
 
+
 	def test_opened_page(self):
 		self.driver.get(urls.sachsenUrl)
+		# looking for 6. wahleriode in the title using selenium
 		self.assertIn('6. Wahlperiode', (self.driver.title))
 
 	def tearDown(self):
