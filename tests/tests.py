aa226b3ddf0c313565a79d792a7fe44a5645bac5
Mani Levian Asli <55234866+manilevian@users.noreply.github.com>
2020-08-22 22:20:55 +0200

    basic_setup
    added urllist file. setUp and tearDown added

--- tests/tests.py
+++ tests/tests.py
@@ -4,3 +4,19 @@ import unittest
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
+import urllist as urls
+
+
+class testing_wikifetcher_sachsen(unittest.TestCase):
+
+	def setUp(self):
+		options = Options()
+		options.headless = False
+		self.driver = webdriver.Firefox(options=options)
+
+	def tearDown(self):
+		self.driver.quit()
+
+bot = testing_wikifetcher_sachsen()
+if __name__ == '__main__':
+	unittest.main()
