e89f14eca82e0a27a7560a766cdc63b3165323df
Mani Levian Asli <55234866+manilevian@users.noreply.github.com>
2020-08-23 20:05:49 +0200

    Soup_sachsen
    fetching the table from the landtags page of sachsen

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ class wikifetcher_sachsen:
         options.headless = False
         self.driver = webdriver.Firefox(options=options)
 
+    def get_politicians_landtag_sachsen(self):
+    	self.driver.get(urls.sachsenUrl)
+    	soup_sachsen = BeautifulSoup(self.driver.page_source, "lxml")
+    	soup_sachsen1 = soup_sachsen.find_all("tbody")[2]
+    	soup_sachsen2 = soup_sachsen1.find_all("tr")
+    	for row in soup_sachsen2:
+    		cols = row.find_all('td')
+    		cols = [x.text.strip() for x in cols]
+    		print(cols)
 
     def quit(self):
         self.driver.quit()
