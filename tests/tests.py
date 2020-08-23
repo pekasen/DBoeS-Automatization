0d8503fe947a7826dbe5e4a42e287770eeebe6cc
Mani Levian Asli <55234866+manilevian@users.noreply.github.com>
2020-08-23 19:12:39 +0200

    add_tests_2
    added couple of tests, checking for type error, checking one output for rico anton, who should be within the outout to ensure the correct output. added main.py configuration.py and urllist for the original file.

--- tests/tests.py
+++ tests/tests.py
@@ -18,12 +18,28 @@ class testing_wikifetcher_sachsen(unittest.TestCase):
 
 	def test_opened_page(self):
 		self.driver.get(urls.sachsenUrl)
-		# looking for 6. wahleriode in the title using selenium
+		# looking for 6. wahleriode in the title using bs4
 		self.assertIn('6. Wahlperiode', (self.driver.title))
+
+	def test_find_name_in_table_to_be_scraped(self):
+		self.driver.get(urls.sachsenUrl)
+		soup = BeautifulSoup(self.driver.page_source, "lxml")
+		fetch_tbody = soup.find_all('tbody')[2]
+		self.assertTrue('title="Rico Anton"' in str(fetch_tbody.find_all('td')[1]))
+
+	@unittest.expectedFailure
+	def test_no_value_error(self):
+		with self.assertRaises(ValueError):
+			self.driver.get(urls.sachsenUrl)
+
+	@unittest.expectedFailure
+	def test_no_type_error(self):
+		with self.assertRaises(TypeError):
+			self.driver.get(urls.sachsenUrl)
 
 	def tearDown(self):
 		self.driver.quit()
 
-bot = testing_wikifetcher_sachsen()
+bot = testing_wikifetcher_sachsen
 if __name__ == '__main__':
 	unittest.main()
