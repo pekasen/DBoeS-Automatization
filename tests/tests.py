2e08227d52d1c286bbd285d96c26ca3ab78ef470
Mani Levian Asli <55234866+manilevian@users.noreply.github.com>
2020-08-31 11:58:41 +0200

    Testfile_from_selenium_to_requests
    Changed the testfile from Selenium to requests

--- tests/tests.py
+++ tests/tests.py
@@ -10,22 +10,12 @@ import urllist as urls
 class testing_wikifetcher_sachsen(unittest.TestCase):
 
 	def setUp(self):
-		options = Options()
-		# Change to 'true' to prevent a firefox window to be opened
-		options.headless = False
-		self.driver = webdriver.Firefox(options=options)
-
-
-	def test_opened_page(self):
-		self.driver.get(urls.sachsenUrl)
-		# looking for 6. wahleriode in the title using bs4
-		self.assertIn('6. Wahlperiode', (self.driver.title))
+		self.r = requests.get(urls.sachsenURL)
 
 	def test_find_name_in_table_to_be_scraped(self):
-		self.driver.get(urls.sachsenUrl)
-		soup = BeautifulSoup(self.driver.page_source, "lxml")
-		fetch_tbody = soup.find_all('tbody')[2]
-		self.assertTrue('title="Rico Anton"' in str(fetch_tbody.find_all('td')[1]))
+		soup = BeutifulSoup(self.r.content, "lxml")
+		fetch_body = soup.find_all('tbody')[2]
+		self.assertTrue('"title="Rico Anton"' in str(fetch_tbody.find_all('td')[1]))
 
 	@unittest.expectedFailure
 	def test_no_value_error(self):
@@ -36,9 +26,6 @@ class testing_wikifetcher_sachsen(unittest.TestCase):
 	def test_no_type_error(self):
 		with self.assertRaises(TypeError):
 			self.driver.get(urls.sachsenUrl)
-
-	def tearDown(self):
-		self.driver.quit()
 
 bot = testing_wikifetcher_sachsen
 if __name__ == '__main__':
