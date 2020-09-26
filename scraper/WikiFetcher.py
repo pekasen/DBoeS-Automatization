12312fd9a4c6d9821510dd658601bd48ba535b8e
Gregor Wiedemann <g.wiedemann@leibniz-hbi.de>
2020-09-26 22:10:55 +0200

    get_politicians function now does not take a predefined table index, but applies the heuristic to extract the largest table from a wiki page

--- scraper/WikiFetcher.py
+++ scraper/WikiFetcher.py
@@ -17,10 +17,15 @@ class Wikifetcher:
     def __init__(self):
         pass
 
-    def get_politicians(self, wiki_url, table_index):
-        wiki_html = pd.read_html(wiki_url)
-        politicians_table = wiki_html[table_index]
-        return politicians_table
+    def get_politicians(self, wiki_url):
+        # read html tables from url
+        html_tables = pd.read_html(wiki_url)
+        # heuristic: assume largest table as table containing all parlamentarians
+        html_tables_lengths = [len(table.index) for table in html_tables]
+        politicians_table_index = html_tables_lengths.index(max(html_tables_lengths))
+        politicians_table = html_tables[politicians_table_index]
+        # return table and table index for logging purposes
+        return politicians_table, politicians_table_index
 
     def fetch_all_parliaments(self):
         # Strip Time from Date to add to every csv file created
@@ -34,8 +39,8 @@ class Wikifetcher:
 
         for parliament, parliament_data in parliaments.items():
             logging.info("Fetching parliament of %s" % parliament_data["name"])
-            politicians_table = self.get_politicians(parliament_data["url"], parliament_data["index"])
-            logging.info("Retrieved %d entries" % politicians_table.shape[0])
+            politicians_table, table_index = self.get_politicians(parliament_data["url"])
+            logging.info("Retrieved %d entries from table %d" % (politicians_table.shape[0], table_index))
             politicians_table.to_csv(output_path + strpdatetoday + "_" + parliament + ".csv", index = False, header = True)
 
 if __name__ == "__main__":
--- test/test_wikifetcher.py
+++ test/test_wikifetcher.py
@@ -26,10 +26,10 @@ class TestParliamentList(unittest.TestCase):
 	def test_c_extracted_parlamentarians(self):
 		fetcher = Wikifetcher()
 		for _, parliament_data in parliaments.items():
-			politicians_table = fetcher.get_politicians(parliament_data["url"], parliament_data["index"])
+			politicians_table, table_index = fetcher.get_politicians(parliament_data["url"])
 			# expect at least 50 politicians per parliament
 			n = len(politicians_table.index)
-			self.assertGreaterEqual(n, 50, "Extracted only %d entries from table %d at %s" % (n, parliament_data["index"], parliament_data["url"]))
+			self.assertGreaterEqual(n, 50, "Extracted only %d entries from table %d at %s" % (n, table_index, parliament_data["url"]))
 
 if __name__ == '__main__':
 	unittest.main()
