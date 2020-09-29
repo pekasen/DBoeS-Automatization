3fa22a8d2d2c8b50bf534200e8d4020d69db59c7
FlxVctr <fvmuench@gmail.com>
2020-09-29 17:46:14 +0200

    fix linter errors, add test for table schemas

--- test/test_wikifetcher.py
+++ test/test_wikifetcher.py
@@ -1,4 +1,4 @@
-import requests 
+import requests
 import unittest
 import sys
 import pandas as pd
@@ -9,26 +9,45 @@ import os
 from scraper.urls import parliaments
 from scraper.WikiFetcher import Wikifetcher
 
+
 class TestParliamentList(unittest.TestCase):
 
-	def url_ok(self, url):
-		r = requests.head(url)
-		return r.status_code == 200
+    def url_ok(self, url):
+        r = requests.head(url)
+        return r.status_code == 200
 
-	def test_a_number_of_parliaments(self):
-		self.assertEqual(len(parliaments), 17) # 16 Landtage + 1 Bundestag 
+    def test_a_number_of_parliaments(self):
+        self.assertEqual(len(parliaments), 17)  # 16 Landtage + 1 Bundestag
 
-	def test_b_wikipage_availability(self):
-		for _, parliament_data in parliaments.items():
-			self.assertTrue(self.url_ok(parliament_data["url"]), "Website not available: %s" % parliament_data["url"])
+    def test_b_wikipage_availability(self):
+        for _, parliament_data in parliaments.items():
+            self.assertTrue(self.url_ok(parliament_data["url"]),
+                            "Website not available: %s" % parliament_data["url"])
 
-	def test_c_extracted_parlamentarians(self):
-		fetcher = Wikifetcher()
-		for _, parliament_data in parliaments.items():
-			politicians_table, table_index = fetcher.get_politicians(parliament_data["url"])
-			# expect at least 50 politicians per parliament
-			n = len(politicians_table.index)
-			self.assertGreaterEqual(n, 50, "Extracted only %d entries from table %d at %s" % (n, table_index, parliament_data["url"]))
+    def test_c_extracted_parlamentarians(self):
+        fetcher = Wikifetcher()
+
+        schema = ['Name', 'Fraktion']
+        for _, parliament_data in parliaments.items():
+            politicians_table, table_index = fetcher.get_politicians(parliament_data["url"])
+            # expect at least 50 politicians per parliament
+            n = len(politicians_table.index)
+
+            self.assertGreaterEqual(n, 50, "Extracted only %d entries from table %d at %s" % (
+                n, table_index, parliament_data["url"]))
+
+            table_schema = politicians_table.columns.values
+
+            self.assertEqual(
+                len(schema), len(table_schema),
+                f"Schema {table_schema} (table {table_index}) doesn't match {schema}."
+            )
+
+            self.assertEqual(
+                schema, politicians_table.columns.values,
+                f"Schema {table_schema} (table {table_index}) doesn't match {schema}."
+            )
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
