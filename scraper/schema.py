637a57aff91764e90a71e37bfc712847b478a077
FlxVctr <fvmuench@gmail.com>
2020-10-21 14:13:36 +0200

    extend and edit schema generation

--- scraper/schema.py
+++ scraper/schema.py
@@ -1,9 +1,25 @@
-schema = [
-    'Name',  # 0
-    'Fraktion',  # 1
-]
+# Configure schema: 'Column_name': ['List', 'of', 'synonyms']
 
-schema_map = {
-    'Mitglied des Landtages': schema[0],
-    'Partei': schema[1]
+columns_with_synonyms = {
+    'Name': ['Mitglied des Landtages'],
+    'Fraktion': ['Partei'],
+    'Wahlkreis': ['Landtagswahlkreis der Direktkandidaten',
+                  'Landtagswahlkreis',
+                  'Wahlkreis/Liste',
+                  ],
+    'Kommentar': ['Anmerkung',
+                  'Anmerkungen',
+                  'Bemerkungen',
+                  ],
 }
+
+
+# Automatically generate schema and synonym map for scraper (dev only)
+
+schema = [column for column in columns_with_synonyms.keys()]
+
+schema_map = {}
+
+for column in columns_with_synonyms.keys():
+    for synonym in columns_with_synonyms[column]:
+        schema_map[synonym] = column
