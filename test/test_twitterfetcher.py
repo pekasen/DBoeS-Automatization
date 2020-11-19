a5ba5a8e8ace3ff2c1468378a5e6c2355d7e7579
FlxVctr <flxvctr@protonmail.com>
2020-11-19 19:46:07 +0100

    unit-test for account_search

--- test/test_twitterfetcher.py
+++ test/test_twitterfetcher.py
@@ -4,7 +4,7 @@ import unittest
 import pandas as pd
 import tweepy
 from scraper.credentials import twitter_api_key, twitter_api_secret_key
-from scraper.twitter_fetcher import OAuthorizer
+from scraper.twitter_fetcher import OAuthorizer, account_search
 
 
 class TestUserSearch(unittest.TestCase):
@@ -26,6 +26,22 @@ class TestUserSearch(unittest.TestCase):
         user = api.verify_credentials()
 
         self.assertIsInstance(user, tweepy.User)
+
+    def test_search_account(self):
+
+        name = "Markus Söder"
+        fields = ['id', 'verified', 'screen_name', 'name', 'description', 'profile_image_url_https']
+
+        result = account_search(name)
+
+        self.assertIsInstance(result, pd.DataFrame)
+
+        for field in fields:
+            self.assertIn(field, result.columns)
+
+        self.assertGreater(len(result), 0)
+
+        self.assertIn('Markus_Soeder', result['screen_name'].values)
 
 
 if __name__ == '__main__':
