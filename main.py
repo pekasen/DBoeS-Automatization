9acdd59020814608e79873918c84d2be5f4f9c29
FlxVctr <flxvctr@protonmail.com>
2020-11-24 15:18:10 +0100

    fix import

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ Main module to run scraping of parlamentarian information from Wikipedia.
 import os
 
 import scraper
+from scraper import twitter_fetcher
 
 if __name__ == "__main__":
 
     if not os.path.isfile('scraper/twitter_tokens.csv'):
-        scraper.twitter_fetcher.OAuthorizer()
+        twitter_fetcher.OAuthorizer()
 
     fetcher = scraper.WikiFetcher()
     fetcher.fetch_all_parliaments()
