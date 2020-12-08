86530f88c5aca5fe41e13d51d2e911c5b08e8dcc
FlxVctr <flxvctr@protonmail.com>
2020-12-08 17:50:05 +0100

    use EntityGroup in main

--- main.py
+++ main.py
@@ -7,7 +7,15 @@ import pandas as pd
 import scraper
 from scraper import twitter_fetcher
 
+from scraper.entities import EntityGroup
+
 if __name__ == "__main__":
+
+    # show all contents of a dataframe
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', -1)
 
     if not os.path.isfile('scraper/twitter_tokens.csv'):
         twitter_fetcher.OAuthorizer()
@@ -24,17 +32,15 @@ if __name__ == "__main__":
         print(f'[{i}]', file)
         i += 1
 
-    print(files)
-
     print('\nPlease select a file by number to retrieve Twitter accounts:')
 
     select = input()
 
-    df = pd.read_csv(f'output/{files[int(select)]}')
-    print(df)
+    parliament = EntityGroup(f'output/{files[int(select)]}')
+    print(parliament.df[['Name', 'id']])
 
     print('\nPlease select a row to retrieve Twitter Accounts:')
 
     select = input()
 
-    print('Retrieving possible accounts for', df['Name'][int(select)])
+    print('Retrieving possible accounts for', parliament.df['Name'][int(select)])
